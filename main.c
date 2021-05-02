7eecfa47b6edb9e46ca5ed247f5e500c55f60036
Yukari <you@example.com>
2021-05-02 16:09:20 +0900

    VA≠PAの対応

--- main.c
+++ main.c
@@ -4,14 +4,50 @@
 
 volatile int value = 10;
 
+static int init(void)
+{
+  /* 以下はリンカスクリプトで定義してあるシンボル 
+   * リンカ・スクリプトで定義されたシンボルを参照可能にする
+   */
+  extern int erodata, data_start, edata, bss_start, ebss;
+
+  /* データ領域とBSS領域を初期化する。この処理以降でないと、 
+   * グローバル変数が初期化されていないので注意
+   */
+  memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+  memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
+
+  /* シリアルデバイスの初期化 */
+  serial_init(SERIAL_DEFAULT_DEVICE);
+  
+  return 0;
+}
+
+int global_data = 0x10; //.dataセクションへ
+int global_bss; //.bssセクションへ
+static int static_data = 0x20; //.dataセクションへ
+static int static_bss; //.bssセクションへ
+
+static void printval(void)
+{
+  puts("global_data= ");  putxval(global_data, 0);  puts("\n");
+  puts("global_bss= ");   putxval(global_bss, 0);   puts("\n");
+  puts("static_data= ");  putxval(static_data, 0);  puts("\n");
+  puts("static_bss= ");   putxval(static_bss, 0);   puts("\n");
+}
+
 int main(void)
 {
-    serial_init(SERIAL_DEFAULT_DEVICE); //シリアルデバイスを初期化
+    init();
     puts("Hello World!\n");
     
-    putxval(value, 0);   puts("\n");
-    value = 20;
-    putxval(value, 0); puts("\n");
+    printval();
+    puts("overwrite variables. \n");
+    global_data = 0x20;
+    global_bss = 0x30;
+    static_data = 0x40;
+    static_bss = 0x50;
+    printval();
 
     while (1) //無限ループ
       ;
